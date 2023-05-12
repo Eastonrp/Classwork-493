07789a1189dc82829b1d7418f9a7d5d8b184287a
Eastonrp <103420703+Eastonrp@users.noreply.github.com>
2023-05-11 19:55:08 -0700

    Update FullDataProcessComplete.py

--- FullDataProcessComplete.py
+++ FullDataProcessComplete.py
@@ -15,6 +15,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from astropy import units as u
 
+sigma_low_threshold = 2
+sigma_high_threshold = 3
+
 data_path = Path(os.getcwd() + '/Documents\ARCSAT_OBSERVATIONS')
 calibrated_data = Path(os.getcwd()+ '/Documents\ARCSAT_OBSERVATIONS/Calibrated')
 calibrated_data.mkdir(exist_ok=True)
@@ -26,10 +29,25 @@ science_images = files.files_filtered(imagetyp = 'Light Frame', include_path = T
 science_size = np.shape(science_images)
 # overscan = '[:2048, :]' #PLACEHOLDER OVERSCAN
 
+raw_bias = files.files_filtered(imagetyp = 'BIAS', include_path = True)
+bias_size = np.shape(raw_bias)
+read_bias = [0] * bias_size[0]
+for i in range(bias_size[0]):
+    read_bias[i] = CCDData.read(raw_bias[i], unit = 'adu')
+    
+combined_bias = ccdp.combine(read_bias,
+                              method='average',
+                              sigma_clip=True, sigma_clip_low_thresh = sigma_low_threshold, sigma_clip_high_thresh = sigma_high_threshold,
+                              sigma_clip_func=np.ma.median, sigma_clip_dev_func=mad_std,
+                              mem_limit=350e8
+                            )
+combined_bias.meta['combined'] = True
+
+combined_bias.write(calibrated_data / 'combined_bias.fit', overwrite = True)
 
 raw_darks = files.files_filtered(imagetyp = 'Dark Frame', include_path = True)
 dark_size = np.shape(raw_darks)
-calibrated_darks = raw_darks
+calibrated_darks = [0] * dark_size[0]
 for i in range(dark_size[0]):
     darker = CCDData.read(raw_darks[i], unit = 'adu') # PLACEHOLDER UNIT, MIGHT BE ELECTRON OR PHOTON
     # overscan_subtracted = ccdp.subtract_overscan(darker, overscan = darker[:, :2048])
@@ -39,7 +57,7 @@ for i in range(dark_size[0]):
 
 combined_darks = ccdp.combine(calibrated_darks,
                               method='average',
-                              sigma_clip=True, sigma_clip_low_thresh=3, sigma_clip_high_thresh=5,
+                              sigma_clip=True, sigma_clip_low_thresh = sigma_low_threshold, sigma_clip_high_thresh = sigma_high_threshold,
                               sigma_clip_func=np.ma.median, sigma_clip_dev_func=mad_std,
                               mem_limit=350e8
                             )
@@ -55,14 +73,11 @@ raw_flats = files.files_filtered(imagetyp = 'FLAT', include_path = True)
 filters = ['sdss_u','sdss_g','sdss_r','sdss_i','sdss_z',]
 
 
-
 variables = {filters[i]: files.files_filtered(imagetyp = 'FLAT',FILTER = filters[i], include_path = True) for i in range(len(filters))}
 for i in range(len(variables)):
     if np.size(variables[filters[i]]) == 0:
         del variables[filters[i]]
 
-
-    
 
 combed_filters = []
 for i in range(len(variables)):
@@ -113,7 +128,7 @@ for i in range(len(combed_filters)):
     
     combined_flatser =  ccdp.combine(variables[combed_filters[i]],
                                   method='average',
-                                  sigma_clip=True, sigma_clip_low_thresh=3, sigma_clip_high_thresh=5,
+                                  sigma_clip=True, sigma_clip_low_thresh = sigma_low_threshold, sigma_clip_high_thresh = sigma_high_threshold,
                                   sigma_clip_func=np.ma.median, sigma_clip_dev_func=mad_std,
                                   mem_limit=350e8
                                 )
@@ -151,10 +166,20 @@ m13_g = g_science[0]
 m13_g = ccdp.ccd_process(m13_g, dark_frame = combined_darks, dark_exposure = 1800*u.s, data_exposure = 120*u.s, master_flat = combined_flats[0])
 m13_g.write(os.getcwd() + '/Documents\ARCSAT_OBSERVATIONS/Calibrated/M13_g.fit', overwrite = True)
 
+m13_r = r_science[0]
+
+m13_r = ccdp.ccd_process(m13_r, dark_frame = combined_darks, dark_exposure = 1800*u.s, data_exposure = 120*u.s, master_flat = combined_flats[1])
+m13_r.write(os.getcwd() + '/Documents\ARCSAT_OBSERVATIONS/Calibrated/M13_r.fit', overwrite = True)
+
 NGC_4726_g = g_science[1]
 
 NGC_4726_g = ccdp.ccd_process(NGC_4726_g, dark_frame = combined_darks, dark_exposure = 1800*u.s, data_exposure = 900*u.s, master_flat = combined_flats[0])
 NGC_4726_g.write(os.getcwd() + '/Documents\ARCSAT_OBSERVATIONS/Calibrated/NGC_4726_g.fit', overwrite = True)
+
+NGC_4726_r = r_science[1]
+NGC_4726_r = ccdp.ccd_process(NGC_4726_g, dark_frame = combined_darks, dark_exposure = 1800*u.s, data_exposure = 900*u.s, master_flat = combined_flats[1])
+NGC_4726_g.write(os.getcwd() + '/Documents\ARCSAT_OBSERVATIONS/Calibrated/NGC_4726_r.fit', overwrite = True)
+
 
 plt.imshow(m13_g)
 
@@ -171,25 +196,3 @@ plt.imshow(m13_g)
 #     final_images[i].write(os.getcwd() + '/Downloads/fitsers/example-cryo-LFC/calibrated/' + + str([i]) +'.fit', overwrite = True)
     
 # plt.imshow(final_images[1])
-# NEEDED IF DARKS DONT INCLUDE BIAS 
-# raw_bias = files.files_filtered(imagetyp = 'BIAS', include_path = True)
-# size = np.shape(raw_bias)
-# calibrated_biases = raw_bias
-# for i in range(size[0]):
-#     biaser = CCDData.read(raw_bias[i], unit = 'adu') # PLACEHOLDER UNIT, MIGHT BE ELECTRON OR PHOTON
-#     overscan_subtracted = ccdp.subtract_overscan(biaser, overscan = biaser[:, :2048])
-#     trimmer = ccdp.trim_image(overscan_subtracted[:, :2048])
-    
-#     calibrated_biases[i] = trimmer
-
-# combined_bias = ccdp.combine(calibrated_biases,
-#                               method='average',
-#                               sigma_clip=True, sigma_clip_low_thresh=5, sigma_clip_high_thresh=5,
-#                               sigma_clip_func=np.ma.median, sigma_clip_dev_func=mad_std,
-#                               mem_limit=350e6
-#                             )
-
-# combined_bias.meta['combined'] = True
-
-# combined_bias.write(calibrated_data / 'combined_bias.fit', overwrite = True)
-
